339448508a64a8d0c0c3be8d4019fe43cf530556
Daniel Liu <liudaniel@qq.com>
2024-02-08 17:54:42 +0800

    setup prettier-plugin-solidity

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,11 +3,13 @@ module.exports = {
   semi: true,
   singleQuote: true,
   trailingComma: 'es5',
+  plugins: ['prettier-plugin-solidity'],
   overrides: [
     {
       files: '*.sol',
       options: {
         bracketSpacing: true,
+        parser: 'solidity-parse',
         printWidth: 160,
         singleQuote: false,
         tabWidth: 4,
