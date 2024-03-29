ddb5b6c3e5115b7e78bf1fb973ee040e75235223
Daniel Liu <liudaniel@qq.com>
2024-03-29 19:08:55 +0800

    setup etherscan for xdc blockchain

--- hardhat.config.js
+++ hardhat.config.js
@@ -140,6 +140,7 @@ module.exports = {
   },
   etherscan: {
     apiKey: {
+      xdcdev: XDC_API_KEY,
       apothem: XDC_API_KEY,
       xinfin: XDC_API_KEY,
       polygonMumbai: POLYGONSCAN_API_KEY,
@@ -150,19 +151,27 @@ module.exports = {
     },
     customChains: [
       {
-        network: 'apothem',
-        chainId: 51,
+        chainId: 551,
+        network: 'xdcdev',
         urls: {
-          apiURL: 'https://api-apothem.blocksscan.io/api',
-          browserURL: 'https://explorer.apothem.network',
+          apiURL: 'https://devnetapi.blocksscan.io/api',
+          browserURL: 'https://devnet.blocksscan.io/',
         },
       },
       {
-        network: 'xinfin',
-        chainId: 50,
+        chainId: 51,
+        network: 'apothem',
         urls: {
-          apiURL: 'https://api-xdc.blocksscan.io/api',
-          browserURL: 'https://explorer.xinfin.network',
+          apiURL: 'https://abapi.blocksscan.io/api',
+          browserURL: 'https://apothembeta.blocksscan.io/',
+        },
+      },
+      {
+        chainId: 50,
+        network: 'xinfin',
+        urls: {
+          apiURL: 'https://bapi.blocksscan.io/api',
+          browserURL: 'https://beta.blocksscan.io/',
         },
       },
     ],
